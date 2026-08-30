e5fe14b43db772ec16453b79c23afb6d14981c60
RahmaAmeen0 <rahma.ameen.sayed@gmail.com>
2026-08-30 09:09:59 +0300

    feat: add identity seeding and migration

---
+++ ELearning.Infrastructure/Persistence/Seeding/AdminSeeder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ELearning.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace ELearning.Infrastructure.Persistence.Seeding
+{
+    public static class AdminSeeder
+    {
+        public static async Task seedAsync(UserManager<User> userManager)
+        {
+            string adminEmail = "admin123@gmail.com";
+            string password = "123ElearningAdmin";
+
+            var admin = await userManager.FindByEmailAsync(adminEmail);
+
+            if(admin == null)
+            {
+                admin = new User
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    EmailConfirmed = true
+                };
+
+                await userManager.CreateAsync(admin, password); 
+            }
+
+            if(! await userManager.IsInRoleAsync(admin, "Admin"))
+            {
+                await userManager.AddToRoleAsync(admin, "Admin");
+            }
+        }
+    }
+}
---
+++ ELearning.Infrastructure/Persistence/Seeding/RoleSeeder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ELearning.Infrastructure.Persistence.Seeding
+{
+    public static class RoleSeeder
+    {
+        public static async Task seedAsync(RoleManager<IdentityRole<int>> roleManager)
+        {
+            string[] roles = ["Admin", "User", "Instructor"];
+
+            foreach(var role in roles)
+            {
+                if(!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole<int>(role));
+                }
+            }
+        }
+    }
+}
